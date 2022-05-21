94441dcdd90edde3527dea7942d22e73ae919da8
zarifikram <65703522+zarifikram@users.noreply.github.com>
2022-05-21 15:51:26 +0600

    new contents added

--- function.py
+++ function.py
@@ -23,36 +23,142 @@
     # print the status of board
     # check whether player has won
 
-def create_a_new_game():
-    pass
 
-def ask_for_input():
-    pass
+# def create_a_new_game():
+#     return [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
 
-def print_board_status():
-    pass
-
-def is_player_won(player_no):
-    pass
-
-board = []
-
-# [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
-# -1 = empty cell
-# 0 = player 1's sign
-# 1 = player 2's sign
-
-create_a_new_game()
-
-NO_OF_INPUTS = 9
-
-for round in range(NO_OF_INPUTS):
-    print_board_status()
-    ask_for_input()
-    if is_player_won(1) == True:
-        print("player 1 has won") 
-        break
-    elif is_player_won(2) == True:
-        print("player 2 has won")
-        break
+# def ask_for_input(board, player_no):
+#     i, j = input("Enter two numbers, seperated by comma").split(",")
     
+#     while board[int(i)][int(j)] != -1:
+#         print("This position is already taken!")
+#         i, j = input("Enter two numbers, seperated by comma").split(",")
+        
+
+#     board[int(i)][int(j)] = player_no - 1
+#     print(board)
+    
+# def print_board_status(board):
+#     res = "\n".join([" | ".join(["O" if val == 1 else "X" if val == 0 else " " for val in row]) for row in board])
+#     print(res)
+
+# def is_player_won(player_no, board):
+#     # horizontal
+#     for row in board:
+#         if row == [player_no - 1 for i in range(3)]:
+#             return True
+
+#     # vertical
+    
+#     for column_no in range(3):
+#         column = []
+#         for row_no in range(3):
+#             column.append(board[row_no][column_no])
+        
+#         if column == [player_no - 1 for i in range(3)]:
+#             return True
+    
+
+#     # diagonal
+#     primary_diagonal = []
+#     secondary_diagonal = []
+#     for i in range(3):
+#         primary_diagonal.append(board[i][i])
+#         secondary_diagonal.append(board[i][2 - i])
+
+#     if primary_diagonal == [player_no - 1 for i in range(3)]:
+#         return True
+
+#     if secondary_diagonal == [player_no - 1 for i in range(3)]:
+#         return True
+
+#     return False
+
+# # def print_board_status(board):
+# #     for row in board:
+# #         row_string = "|"
+# #         for item in row:
+# #             if item == -1:
+# #                 row_string += " "
+# #             elif item == 0:
+# #                 row_string += "O"
+# #             else:
+# #                 row_string += "X"
+
+# #             row_string += "|"
+        
+# #         print(row_string)
+
+# board = []
+
+# # [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+# # -1 = empty cell
+# # 0 = player 1's sign
+# # 1 = player 2's sign
+
+# board = create_a_new_game()
+# # print_board_status(board)
+# NO_OF_INPUTS = 9
+# player_no = 1 #player 1 starts first
+
+# for round in range(NO_OF_INPUTS):
+#     print_board_status(board)
+#     ask_for_input(board, player_no)
+
+#     if player_no == 1:
+#         player_no = 2
+#     else:
+#         player_no = 1
+
+#     if is_player_won(1, board) == True:
+#         print("player 1 has won") 
+#         break
+#     elif is_player_won(2, board) == True:
+#         print("player 2 has won")
+#         break
+    
+#  |  | 
+#  |  |
+#  |  |
+
+
+# X | O | X
+# O | X | O
+# X | O | X
+
+# task 1
+# take i and j from user. Create a list (length i) of lists( length j) with user specified value
+# example 2, 4
+# 1 2 3 4 5 6 7 8 
+# OUTPUT
+# [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+# task 2
+# take a list from user (basically task 1)
+# add(or subtract or multiply or divide) a value to the list
+# 2, 4
+# 1 2 3 4 5 6 7 8 
+# 5
+# [[6, 7, 8, 9], [10, 11, 12, 13]]
+
+i = int(input())
+j = int(input())
+
+output_list = []
+
+for row_no in range(i):
+    row = []
+    for column_no in range(j):
+        user_value = int(input())
+        row.append(user_value)
+    output_list.append(row)
+
+print(output_list)
+
+value_to_be_added = int(input())
+
+for row in output_list:
+    for i, value in enumerate(row):
+        row[i] = row[i] + value_to_be_added
+
+print(output_list)
---
+++ numpy_tutorial.py
@@ -0,0 +1,214 @@
+from random import random
+import numpy as np
+
+# turn a list into numpy
+# random_list = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+# random_numpy_list = np.array(random_list)
+
+# print(random_list)
+# print(random_numpy_list.shape)
+
+# creating a diagonal matrix
+# diagonal_matrix = np.eye(4)
+# print(diagonal_matrix)
+
+# create a list filled zeros
+# matrix = np.zeros((5, 4))
+# print(matrix)
+
+# create a list filled w ones
+# matrix = np.ones((2, 5))
+# print(matrix)
+# for i in range(matrix.shape[0]):
+#     for j in range(matrix.shape[1]):
+#         matrix[i][j] = int(input())
+
+# print(matrix)
+
+# matrix = matrix + 5
+# print(matrix)
+
+# 2
+# 3
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# OUTPUT
+# 1 2 3
+# 4 5 6
+# 3
+# 1
+# 1
+# 2
+# 3
+# OUTPUT
+# 1
+# 2
+# 3
+
+# OUTPUT
+# TBD
+# TBD
+
+# Matrix Multiplication
+
+# nRow, nCol = input('Row, Column : ').split(",")
+# nRow = int(nRow)
+# nCol = int(nCol)
+
+# matrix1 = np.ones((nRow, nCol))
+# for i in range(nRow):
+#     for j in range(nCol):
+#         matrix1[i][j] = int(input())
+
+
+# nRow, nCol = input('Row, Column : ').split(",")
+# nRow = int(nRow)
+# nCol = int(nCol)
+
+# matrix2 = np.ones((nRow, nCol))
+# for i in range(nRow):
+#     for j in range(nCol):
+#         matrix2[i][j] = int(input())
+
+
+
+
+# resultant_matrix = np.ones((matrix1.shape[0], matrix2.shape[1]))
+
+# for i in range(matrix1.shape[0]):
+    # for j in range(matrix2.shape[1]):
+        # sum = 0
+        # resultant_matrix[i][j] = matrix1[i, :].dot(matrix2[:, j])
+        # for k in range(matrix1.shape[1]):
+        #     sum += (matrix1[i][k] * matrix2[k][j])
+
+        # resultant_matrix[i][j] = sum
+
+# using np function
+# resultant_matrix = matrix1.dot(matrix2)
+# print(matrix1)
+# print(matrix2)
+# print(resultant_matrix)
+
+# operations in numpy
+# a = np.ones(5)
+# b = np.ones(5)
+
+# for i in range(5):
+#     a[i] = int(input())
+
+# for i in range(5):
+#     b[i] = int(input())
+
+
+# print(a)
+# print(b)
+# print(a + b)
+
+
+# numpy diagonal matrix with user specified values
+# print(np.diag([2, 3, 1, 10]))
+
+# creates a list with 5 entries with numbers from 1 to 100
+# a_list_with_ints = np.random.randint(1, 100, 5)
+# creates a list with 5 entries from 0. to 1.
+# a_list_with_floats = np.random.rand(5)
+# cretes a list with 5 entries with floats (-ve including)
+# a_list_with_negative_floats = np.random.randn(5)
+# print(a_list_with_negative_floats)
+
+# reshaping
+# a = np.random.randint(1, 10, 8)
+# a = a.reshape((2, 4))
+# reshaping with -1 takes away the calculation
+# a = a.reshape((2, -1))
+# print(a)
+
+# create a 10 X 5 matrix that has values from 0. to 10.
+
+# astype changes the type of the list
+# a = np.random.rand(5)
+# print(a)
+# a = a.astype("int32")
+# print(a)
+
+# create a random list that has zero in the last row and last column
+# random_list = np.random.rand(16).reshape((4,-1))
+# print(random_list)
+# random_list[3] = 0
+# random_list[:, 3] = 0
+# print(random_list)
+# create a list that looks like this
+
+# 1 0 0 0 
+# 1 1 0 0
+# 1 1 1 0
+# 1 1 1 1
+
+# replace 1 with random number
+# random_list = np.random.rand(16).reshape((4,-1))
+# for row_no in range(random_list.shape[0]):
+#     random_list[row_no, row_no + 1 : ] = 0
+
+# print(random_list)
+
+
+# Random Password Generator
+# user will give you length
+# for that length generate a password that
+# i) includes alphabet (upper + lower)
+# ii) includes number
+# iii)includes special symbols
+
+# import random
+
+# alphabets_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# alphabets_lower = alphabets_upper.lower()
+# numbers = "1234567890"
+# symbols = "@#$%^&*()_+-="
+# characters = alphabets_lower + alphabets_upper + numbers + symbols
+
+# length_of_password = int(input("length :        "))
+# # 8
+# password_list = []
+# upper_char = alphabets_upper[random.randint(0, len(alphabets_upper) - 1)]
+# password_list.append(upper_char)
+# lower_char = alphabets_lower[random.randint(0, len(alphabets_lower) - 1)]
+# password_list.append(lower_char)
+# number_char = numbers[random.randint(0, 9)]
+# password_list.append(number_char)
+# symbol_char = symbols[random.randint(0, len(symbols) - 1)]
+# password_list.append(symbol_char)
+
+# characters_indices = np.random.randint(0, len(characters) - 1, length_of_password - 4)
+# for character_index in characters_indices:
+#     password_list.append(characters[character_index])
+
+
+# np.random.shuffle(password_list)
+# password = "".join(password_list)
+# print(password)
+
+
+
+# USING NUMPY CONDITIONS
+# you will get a numpy list of integers
+# create a new list from that list where every value is greater than 10
+
+# random_list = np.random.randint(0, 50, 10)
+
+# print(random_list)
+# # print(random_list > 10)
+# # print(random_list[[False, True, False, True]])
+# # print(random_list[random_list > 10])
+
+# print(random_list[])
+
+# get a list
+# create another list from that which only has the even values
+# random_list[random_list % 2 == 0] = 0
+# print(random_list)
